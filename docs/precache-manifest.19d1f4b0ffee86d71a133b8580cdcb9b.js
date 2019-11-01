568c78b716567563a1e1f4b5cc3e21fabf1a853e
Hiromu OCHIAI <otiai10@gmail.com>
2019-11-01 20:36:43 +0900

    Update homepage as "."

---
+++ docs/precache-manifest.19d1f4b0ffee86d71a133b8580cdcb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c483a4eef5b700122fa7669f18bd0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "298fc78839a1c6974112",
+    "url": "./static/css/main.82c1efa6.chunk.css"
+  },
+  {
+    "revision": "a9a7bfd025394c63925b",
+    "url": "./static/js/2.9c4e8a28.chunk.js"
+  },
+  {
+    "revision": "298fc78839a1c6974112",
+    "url": "./static/js/main.64e9cc8e.chunk.js"
+  },
+  {
+    "revision": "97572384fa05a671ba05",
+    "url": "./static/js/runtime-main.e122ef8b.js"
+  },
+  {
+    "revision": "2723d63efa5eba6bc143fc14a9aa0d97",
+    "url": "./static/media/camera.2723d63e.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a9bb16c872cbfc692ccc554f5f93307.js"
+  "./precache-manifest.19d1f4b0ffee86d71a133b8580cdcb9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
