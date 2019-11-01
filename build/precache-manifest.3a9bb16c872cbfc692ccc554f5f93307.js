f86cb937c729107a04a8193f403da705ec9b74cc
Hiromu OCHIAI <otiai10@gmail.com>
2019-11-01 20:24:06 +0900

    Include build folder to git repo

---
+++ build/precache-manifest.3a9bb16c872cbfc692ccc554f5f93307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f1a5fd12abc4c5a1e69596fedf5fa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38d2306c33b80060f6af",
+    "url": "/static/css/main.82c1efa6.chunk.css"
+  },
+  {
+    "revision": "a9a7bfd025394c63925b",
+    "url": "/static/js/2.9c4e8a28.chunk.js"
+  },
+  {
+    "revision": "38d2306c33b80060f6af",
+    "url": "/static/js/main.b222413b.chunk.js"
+  },
+  {
+    "revision": "000bcb13ef1ea9fc6ebf",
+    "url": "/static/js/runtime-main.b8c8a532.js"
+  },
+  {
+    "revision": "2723d63efa5eba6bc143fc14a9aa0d97",
+    "url": "/static/media/camera.2723d63e.svg"
+  }
+]);
