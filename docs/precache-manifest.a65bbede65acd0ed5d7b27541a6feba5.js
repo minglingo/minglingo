3e3f7b7038c4a4a485309b4284bbe644b8fcdaf9
Hiromu OCHIAI <otiai10@gmail.com>
2019-11-10 15:04:02 -0300

    Change project name and deploy

---
+++ docs/precache-manifest.a65bbede65acd0ed5d7b27541a6feba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6264bf2f898455feecb31d13c373f081",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76ad8fb75e57b61bf9f5",
+    "url": "./static/css/main.22f97984.chunk.css"
+  },
+  {
+    "revision": "bc8aaf71b3a03b5c3792",
+    "url": "./static/js/2.6fefb470.chunk.js"
+  },
+  {
+    "revision": "76ad8fb75e57b61bf9f5",
+    "url": "./static/js/main.022c49c4.chunk.js"
+  },
+  {
+    "revision": "b7713116e9d434b58b6c",
+    "url": "./static/js/runtime-main.1091f14c.js"
+  },
+  {
+    "revision": "2723d63efa5eba6bc143fc14a9aa0d97",
+    "url": "./static/media/camera.2723d63e.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.19d1f4b0ffee86d71a133b8580cdcb9b.js"
+  "./precache-manifest.a65bbede65acd0ed5d7b27541a6feba5.js"
 );
 
 self.addEventListener('message', (event) => {
