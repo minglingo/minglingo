d1edd45cadeb984470eebef3c10ea029d079ae5d
Hiromu OCHIAI <otiai10@gmail.com>
2019-11-11 18:45:53 -0300

    Add QRCode generate script

---
+++ scripts/generate.js
@@ -0,0 +1,12 @@
+const qrcode = require('qrcode');
+const config = require('../src/config.json');
+
+const token = config.application.token || config.application.name;
+const action = 'detected';
+(config.bingo.slot.variations || []).map(async (v) => {
+    const encoded = JSON.stringify({token, action, payload: {value: v.value}});
+    const fpath = `./qrcodes/${v.value}.png`;
+    await qrcode.toFile(fpath, encoded);
+    console.log(`File ${fpath} generated`)
+});
+
