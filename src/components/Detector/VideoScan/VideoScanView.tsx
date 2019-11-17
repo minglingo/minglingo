58c07766c90bd29741968803c19e9af01fd943c5
Hiromu OCHIAI <otiai10@gmail.com>
2019-11-17 17:15:35 -0300

    Fix video view design

--- src/components/Detector/VideoScan/VideoScanView.tsx
+++ src/components/Detector/VideoScan/VideoScanView.tsx
@@ -1,46 +1,80 @@
-import React, { createRef } from 'react';
+import React, { createRef, useState, RefObject, useEffect } from 'react';
 import "./VideoScanView.scss";
 
-import camera from "../camera.svg";
 import { detectQRCodeFromVideo } from '../../../services/Detector';
 import { QRCodeData } from '../../../models/qrcode';
+import { QRCode } from 'jsqr';
 
-const [vw, vh] = [80, 80];
+import left from './caret-left.svg';
 
 const isMobile = /(iPhone|iPad|iPod|Android)/i.test(navigator.userAgent);
 const facingMode =  isMobile ? { exact: 'environment' } : 'user';
-const constraints = { audio: false, video: { width: vw, height: vh, facingMode } };
+
+const detect = (video: HTMLVideoElement, callback: (qrcode: QRCode | null) => void) => {
+    const canvas = document.createElement('canvas');
+    [canvas.width, canvas.height] = [video.width, video.height];
+    const qrcode = detectQRCodeFromVideo({ video, canvas })
+    callback(qrcode);
+};
+
+const VideoStream: React.FC<{
+    stream: MediaStream,
+    stop: (s: MediaStream, data?: QRCodeData) => void,
+    video: RefObject<HTMLVideoElement>,
+}> = ({ stream, stop, video }) => {
+    const onSuccess = (data: QRCodeData) => {
+        if (video.current) video.current.pause();
+        alert(JSON.stringify(data));
+        stop(stream, data);
+    };
+    useEffect(() => {
+        const v = video.current;
+        if (!v) return console.error('video tag is not ready');
+        v.srcObject = stream;
+        let id: NodeJS.Timeout;
+        const callback = (qrcode: QRCode | null) => {
+            if (!qrcode) return;
+            clearInterval(id);
+            onSuccess(JSON.parse(qrcode.data) as QRCodeData);
+        };
+        id = setInterval(detect.bind(null, v, callback), 1 * 1000);
+    }, []);
+    return (
+        <div className="Video_Stream_Container">
+            <div className="Video_Stream_Navigation Top">
+                <div className="nav-left" onClick={() => stop(stream)}>
+                    <img src={left} alt="back" className="nav-item-img" />
+                    <span>Back</span>
+                </div>
+                <div className="nav-title">Scan QR code</div>
+                <div className="nav-right"></div>
+            </div>
+            <div>
+                <video ref={video} muted={true} autoPlay playsInline width={'100%'} height={'100%'} />
+            </div>
+            <div className="Video_Stream_Navigation Bottom">
+                <div className="nav-title">Place code inside the box</div>
+            </div>
+        </div>
+    );
+};
 
 const VideoScanView: React.FC<{ punch(data: QRCodeData): void }> = ({ punch }) => {
-    const vref = createRef<HTMLVideoElement>();
-    const canvas = document.createElement('canvas');
-    canvas.width = vw; canvas.height = vh;
-    let detectorInterval: NodeJS.Timeout;
-    const detect = (video: HTMLVideoElement, stream: MediaStream) => {
-        const qrcode = detectQRCodeFromVideo({ video, canvas })
-        if (!qrcode) return;
-        clearInterval(detectorInterval);
-        video.pause();
-        stream.getTracks().map(track => track.stop());
-        punch(JSON.parse(qrcode.data));
+    const [stream, setStream] = useState<MediaStream>();
+    const stop = (s: MediaStream, data?: QRCodeData) => {
+        setStream(undefined);
+        s.getTracks().map(track => track.stop())
+        if (data) punch(data);
     };
-    const startVideoScanning = async () => {
-        if (!vref.current) return;
-        vref.current.play();
-        vref.current.setAttribute('muted', '');
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        vref.current.srcObject = stream;
-        detectorInterval = setInterval(() => detect(vref.current as HTMLVideoElement, stream), 1000);
+    const v = createRef<HTMLVideoElement>();
+    const onClickStart = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode } });
+        setStream(stream);
     };
     return (
         <div className="video-capture-container">
-            <button>SCAN QR CODE</button>
-            {/* <div>
-                <video ref={vref} autoPlay playsInline width={vw} height={vh} />
-            </div>
-            <div onClick={startVideoScanning}>
-                <img src={camera} alt="Star Scanning QR" />
-            </div> */}
+            <button onClick={onClickStart}>SCAN QR CODE</button>
+            {stream ? <VideoStream stream={stream} stop={stop} video={v} /> : null}
         </div>
     );
 };
